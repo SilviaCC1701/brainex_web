da2b1e032056f76a7dfb4e107a5a7e29a9edf160
SilviaCC1701 <silviacc1701@gmail.com>
2025-05-23 20:20:21 +0200

    add Controllers y vistas

---
+++ BrainEx/Controllers/CalcBrainAgeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrainEx.Controllers
+{
+    public class CalcBrainAgeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BrainEx/Controllers/JuegosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrainEx.Controllers
+{
+    public class JuegosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
